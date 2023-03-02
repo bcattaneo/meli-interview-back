617a6e3737ca548e481397b724cea3c377e77151
bcattaneo <c@ttaneo.uy>
2023-03-02 10:26:12 -0300

    feat: make subscription a JPA entity

--- src/main/java/com/meli/interview/back/subscription_api/subscription/Subscription.java
+++ src/main/java/com/meli/interview/back/subscription_api/subscription/Subscription.java
@@ -1,25 +1,58 @@
 package com.meli.interview.back.subscription_api.subscription;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
 public class Subscription {
 
+  @Id
+  @SequenceGenerator(
+      name = "subscription_sequence",
+      sequenceName = "subscription_sequence",
+      allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subscription_sequence")
+  private Long id;
+
+  @NonNull
+  @Column(unique = true)
   private String partner;
 
-  public float getPrice() {
-    float price = 0;
-    if (partner.equals("disney")) {
-      price = 100;
-    }
+  @NonNull private Float price;
 
-    if (partner.equals("netflix")) {
-      price = 200;
-    }
-
-    if (partner.equals("spotify")) {
-      price = 50;
-    } else {
-      price = 0;
-    }
-
-    return price;
-  }
+  //  public float getPrice() {
+  //    float price = 0;
+  //    if (partner.equals("disney")) {
+  //      price = 100;
+  //    }
+  //
+  //    if (partner.equals("netflix")) {
+  //      price = 200;
+  //    }
+  //
+  //    if (partner.equals("spotify")) {
+  //      price = 50;
+  //    } else {
+  //      price = 0;
+  //    }
+  //
+  //    return price;
+  //  }
 }
